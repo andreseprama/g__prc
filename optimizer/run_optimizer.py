b4b4b3f8a0d80a8a60e514be04c9460520efa842
Andre Couto <andre.couto@seprama.pt>
2025-06-17 11:10:01 +0100

    Commit automático às 11:10

--- optimizer/run_optimizer.py
+++ optimizer/run_optimizer.py
@@ -80,9 +80,9 @@ async def optimize(sess: AsyncSession, dia: date, registry_trailer: Optional[str
         )
 
         search = pywrapcp.DefaultRoutingSearchParameters()
-        search.time_limit.seconds = 30
-        search.log_search = debug
-        search.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.SAVINGS
+        search.log_search = True
+        search.time_limit.seconds = 60
+        search.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
         search.local_search_metaheuristic = routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
 
         solution = routing.SolveWithParameters(search)
